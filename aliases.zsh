55325af4a4d7911fbf2108a74ff59f4777f2343b
Tawukt <yadnin.git@hotmail.com>
2026-09-20 03:49:40 +0200

    added some aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,6 +1,6 @@
 alias vim=nvim
 alias l='eza -lahg --sort=name --icons=auto --group-directories-first'
-alias ll='eza -lh --sort=name --icons=auto --group-directories-first'
+alias ll='eza -ah --sort=name --icons=auto --group-directories-first'
 alias ls='eza --sort=name --icons=auto --group-directories-first'
 alias lt='eza --sort=name --icons=auto --group-directories-first --tree'
 
@@ -9,6 +9,7 @@ alias ...='cd ../../'
 alias .3='cd ../../..'
 alias .4='cd ../../../..'
 alias .5='cd ../../../../..'
+alias -- -='cd -'
 
 alias p="pacman"
 alias t="tmux"
@@ -21,6 +22,8 @@ alias lsblk="lsblk | bat --style=plain -l conf"
 alias ps="ps | bat --style=plain -l conf" 
 alias lsusb="lsusb | bat --style=plain -l conf"
 alias df="df -h | bat --style=plain -l conf"
+
+alias diff="diff --color=auto"
 
 
 [[ "$TERM" == "xterm-kitty" ]] && alias ssh="TERM=xterm-256color ssh"
